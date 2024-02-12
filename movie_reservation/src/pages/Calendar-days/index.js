23f6a63b475fed524892fa996734fd51a46c5632
AWSearl <ouanoearl.aws@gmail.com>
2024-02-12 16:42:12 +0800

    Committed Code

--- movie_reservation/src/pages/Calendar-days/index.js
+++ movie_reservation/src/pages/Calendar-days/index.js
@@ -2,57 +2,57 @@ import MovieSelected from '../../components/MovieSelected';
 import ProtectedRoute from '../../components/ProtectedRoute';
 import Home from '../Home';
 
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 export default function CalendarDays(props) {
-    let firstDayOfMonth = new Date(props.day.getFullYear(), props.day.getMonth(), 1);
-    let weekdayOfFirstDay = firstDayOfMonth.getDay();
-    let currentDays = [];
+  const navigate = useNavigate();
 
-    for (let day = 0; day < 42; day++) {
-        if (day === 0 && weekdayOfFirstDay === 0) {
-          firstDayOfMonth.setDate(firstDayOfMonth.getDate() - 7);
-        } else if (day === 0) {
-          firstDayOfMonth.setDate(firstDayOfMonth.getDate() + (day - weekdayOfFirstDay));
-        } else {
-          firstDayOfMonth.setDate(firstDayOfMonth.getDate() + 1);
-    }
-    
-        let calendarDay = {
-          currentMonth: (firstDayOfMonth.getMonth() === props.day.getMonth()),
-          date: (new Date(firstDayOfMonth)),
-          month: firstDayOfMonth.getMonth(),
-          number: firstDayOfMonth.getDate(),
-          selected: (firstDayOfMonth.toDateString() === props.day.toDateString()),
-          year: firstDayOfMonth.getFullYear()
-        }
-    
-        currentDays.push(calendarDay);
-    }
-  
-    return (
-        <div className="table-content">
-        {
-          currentDays.map((day, index) => {
-            return (
-              <div key={index} className={"calendar-day" + (day.currentMonth ? " current" : " not") + (day.selected ? " selected" : "")}
-                    onClick={() => { 
-                      props.changeCurrentDay(day) 
-                      {console.log(day)} 
-                      } 
-                    }
-              > 
-                    {/* add the route */}
-                       {/* <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>}/> */}
-                       {/* <Route path="/Niles" element={<ProtectedRoute><MovieSelected/></ProtectedRoute>}/> */}
-                <p>{day.number}</p>
-              </div>
-            )
-          })
-        }
-      </div>
-    )
+  let firstDayOfMonth = new Date(props.day.getFullYear(), props.day.getMonth(), 1);
+  let weekdayOfFirstDay = firstDayOfMonth.getDay();
+  let currentDays = [];
+
+  for (let day = 0; day < 42; day++) {
+      if (day === 0 && weekdayOfFirstDay === 0) {
+        firstDayOfMonth.setDate(firstDayOfMonth.getDate() - 7);
+      } else if (day === 0) {
+        firstDayOfMonth.setDate(firstDayOfMonth.getDate() + (day - weekdayOfFirstDay));
+      } else {
+        firstDayOfMonth.setDate(firstDayOfMonth.getDate() + 1);
   }
+  
+      let calendarDay = {
+        currentMonth: (firstDayOfMonth.getMonth() === props.day.getMonth()),
+        date: (new Date(firstDayOfMonth)),
+        month: firstDayOfMonth.getMonth(),
+        number: firstDayOfMonth.getDate(),
+        selected: (firstDayOfMonth.toDateString() === props.day.toDateString()),
+        year: firstDayOfMonth.getFullYear()
+      }
+  
+      currentDays.push(calendarDay);
+  }
+
+  return (
+      <div className="table-content">
+      {
+        currentDays.map((day, index) => {
+          return (
+            <div key={index} className={"calendar-day" + (day.currentMonth ? " current" : " not") + (day.selected ? " selected" : "")}
+                  onClick={() => { 
+                    props.changeCurrentDay(day) 
+                    {console.log(day.date)} 
+                      navigate('/gi', { state: { selectedDay: day }})
+                    } 
+                  }
+            > 
+              <p>{day.number}</p>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
 
 
 
