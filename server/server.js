dacdb290bb522cb5aae5afd837691506df082aee
AWSearl <ouanoearl.aws@gmail.com>
2024-02-12 15:39:34 +0800

    server changed

--- server/server.js
+++ server/server.js
@@ -1,9 +1,34 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
-const port = process.env.PORT || 5000;
-//Since we are using .env environment variables
-require("dotenv").config();
-//Add the DB Config
-const dbConfig = require("./config/dbConfig");
-//let us now start the server
-app.listen(port, () => console.log(`Node JS Server is running on port ${port}`));
+const PORT = 3000;
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost:27017/movieDB', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Define a Movie schema
+const movieSchema = new mongoose.Schema({
+  title: String,
+  genre: String,
+  director: String,
+  releaseYear: Number
+});
+
+// Create a Movie model
+const Movie = mongoose.model('Movie', movieSchema);
+
+// Define a route to get all movies
+app.get('/movies', async (req, res) => {
+  try {
+    const movies = await Movie.find({});
+    res.json(movies);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
