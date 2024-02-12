2e4d64ff7506268d9050a5818d07ea3b3e14424d
AWSearl <ouanoearl.aws@gmail.com>
2024-02-12 11:12:11 +0800

    app.js

--- movie_reservation/src/App.js
+++ movie_reservation/src/App.js
@@ -7,29 +7,16 @@ import Home from './pages/Home';
 import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
-  return (
-  <div>
-<<<<<<< HEAD
-  <BrowserRouter>
-  <Routes>
-  {/* <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>}/> */}
-  <Route path="movies" element={<ProtectedRoute><MovieList/></ProtectedRoute>}/>
-  </Routes>
-  </BrowserRouter>
-  <main className='Calendar'><Calendar/></main>
-=======
-  <Router>
-    <Routes>
-    {/* <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>}/> */}
-    {/* <Route path="movies" element={<ProtectedRoute><MovieList/></ProtectedRoute>}/> */}
-      <Route exact path='/' Component={Calendar} />
-      {/* <Route path='/movies' Component={MovieList} />
-      <Route path='/movies/:id' Component={MovieChosen} /> */}
-    </Routes>
-  </Router>
-  
->>>>>>> c30d491f4631a39135c12baf3456cb8921a75dbb
-  </div>
-  );
+    return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/" element={<ProtectedRoute><Home/></ProtectedRoute>}/> */}
+          <Route path="movies" element={<ProtectedRoute><MovieList/></ProtectedRoute>}/>
+          </Routes>
+        </BrowserRouter>
+      <main className='Calendar'><Calendar/></main>
+    </div>
+    );
   }
 export default App;
